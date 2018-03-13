61429f96099a394bf4d9a9eb077667d12a3189b3
ArturDaukaev <daukaevar@bashneft.ru>
2018-03-13 18:18:06 +0500

    Add small improvements

--- lesson1/right_triangle.rb
+++ lesson1/right_triangle.rb
@@ -22,9 +22,17 @@ sides << c_side
 # maximum number will be in last position
 sides.sort!
 
-if (sides[2] ** 2) == (sides[0] ** 2) + (sides[1] ** 2) && sides.uniq.size == 2
-	puts "Triangle is right and is isosceles!"
-elsif (sides[2] ** 2) == (sides[0] ** 2) + (sides[1] ** 2)
-	puts 'Triangle is right'
+# calculcate triangle rectangular 
+# and write value into the rectangular_triangle variable
+rectangular_triangle = (sides[2]**2) == (sides[0]**2) + (sides[1]**2)
+
+if rectangular_triangle && sides.uniq.size == 2
+	puts "Triangle is rectangular and is isosceles!"
+elsif rectangular_triangle
+	puts 'Triangle is rectangular.'
+elsif sides.uniq.size == 1
+	puts 'Triangle is rectangular and equilateral.'
+else
+	puts 'triangle unknown'
 end
 
