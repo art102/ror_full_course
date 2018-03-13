732a88911fa767c444c3da9e1c38b8e7355b1e54
ArturDaukaev <daukaevar@bashneft.ru>
2018-03-13 09:43:45 +0500

    Create app for calculating area of a triangle

---
+++ lesson1/triangle_area.rb
@@ -0,0 +1,11 @@
+puts 'OK, now we are going to calculate area of a triangle.'
+
+print 'Enter base: '
+base = gets.strip.to_i
+
+print 'Enter hight: '
+hight = gets.strip.to_i
+
+area = (base / 2) * hight
+
+puts "Area of the triangle: #{area}"
