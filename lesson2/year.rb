0a132da9b17350c14aadbb6a6a25a2e5e97b30d4
Artur Daukaev <mckorven@gmail.com>
2018-03-15 23:58:32 +0500

    some changes

--- lesson2/year.rb
+++ lesson2/year.rb
@@ -17,12 +17,12 @@ days_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 # write to leap_year true if
 # year is leap-year and false if not
 leap_year = date[2] % 4 == 0 ? true : false
-# If now is leap-year then
+# If now is leap-year, then
 # change value of second element array
 days_of_month[1] = 29 if leap_year
 
 # If day and month are equal 1
-# amount of day 2
+# amount of day 1
 if date[0] == 1 && date[1] == 1
   index_number = 1
 # If date more than 12
