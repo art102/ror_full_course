84605ad015364456e06c7e8981d311bd5bb015c7
ArturDaukaev <daukaevar@bashneft.ru>
2018-03-16 17:27:44 +0500

    Add comments

--- lesson2/purchases_sum.rb
+++ lesson2/purchases_sum.rb
@@ -10,8 +10,10 @@
 Вычислить и вывести на экран итоговую сумму всех покупок в "корзине". 
 =end
 
+# create empty hash for items
 cart = {} 
 
+# Create loop where get info from user
 loop do
   print 'What would you like to buy? (enter "stop" if you finished): '
   item_name = gets.strip.downcase
@@ -23,17 +25,24 @@ loop do
   print 'Enter amount: '
   amount = gets.strip.to_f
 
+  # add item name, price and amount to "cart" hash
   cart[item_name] = { price => amount }
 end
 
+#create empty array for total cost of cart
 cost_of_cart = []
+
+# get values of hash
 cart.each do |item, price|
   price.each do |val, amount|
+    # get cost of many items of certain kind
     v = val * amount
+    # add cost to array
     cost_of_cart << v
     puts "#{item} - #{v} dollars."
   end 
 end
+#get cost sum of all items 
 total_cost = cost_of_cart.inject(0) { |one, two| one + two }
 
 puts "Cost of purchases in the cart: #{total_cost} dollars."
