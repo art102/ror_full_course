b6fe6a64b5e9b912d6273562c6b61f65f04e322a
Artur Daukaev <mckorven@gmail.com>
2018-03-17 21:16:37 +0500

    First refactoring

--- lesson2/fibonacci.rb
+++ lesson2/fibonacci.rb
@@ -2,8 +2,9 @@ fibonacci_row = [1, 1]
 
 loop do
   number = fibonacci_row[-1] + fibonacci_row[-2]
+  break if number > 100
   fibonacci_row << number
-  break if fibonacci_row.last > 100
+  
 end
 
 puts 'Thihs is fibonacci row:'
