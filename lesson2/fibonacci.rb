72c8b9aa8789ba38b20e1e5296b537a3075bff1a
Art102 <mckorven@gmail.com>
2018-03-19 00:53:05 +0500

    Reafctor fibonacci.rb

--- lesson2/fibonacci.rb
+++ lesson2/fibonacci.rb
@@ -1,11 +1,12 @@
 fibonacci_row = [1, 1]
 
-while fibonacci_row.last < 100
+while fibonacci_row.max < 100
   number = fibonacci_row[-1] + fibonacci_row[-2]
-  fibonacci_row << number
-  
+  fibonacci_row << number 
 end
 
+fibonacci_row.delete(fibonacci_row.max) if fibonacci_row.max > 100
+
 puts 'This is fibonacci row:'
-print fibonacci_row.first(fibonacci_row.size-1)
-puts ""
+p fibonacci_row
+
