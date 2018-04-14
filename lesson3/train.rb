0bddffe0696a2348a60b415ea582d485a671b380
Art102 <mckorven@gmail.com>
2018-04-14 21:18:54 +0500

    Edit next and prev methods, edit currents_station, add new methods

--- lesson3/train.rb
+++ lesson3/train.rb
@@ -39,48 +39,60 @@ class Train
   def accept_route(route)
     #@route.stations[0].add_train(self)
     @route = route
-    @current_station = @route.stations.first 
-    puts "Train is on the #{@current_station.name} and train is moving on the route #{@route.stations.first.name} - #{@route.stations.last.name}"
+    @current_station = 0
+    puts "Train is on the #{@route.stations[@current_station]} and train is moving on the route #{@route.stations.first.name} - #{@route.stations.last.name}"
   end
 
+  # show next station on the route
   def next_station
-    @next_station = @route.stations.index(@current_station) + 1 if @current_station != @route.stations.last
-    puts "Next station is #{@route.stations[@next_station].name}."
+    @next_station = @route.stations[@current_station + 1] if @route.stations[@current_station] != @route.stations.last
   end
 
+  # show previous station on the route
   def prev_station
-    @prev_station = @route.stations.index(@current_station) - 1 if @current_station != @route.stations.first
+    @prev_station = @route.stations[@current_station - 1] if @route.stations[@current_station] != @route.stations.first
     # puts @prev_station
     # #puts "Previous station is #{@route.stations[@prev_station].name}."
   end
 
-  def print_prev_station
-    if prev_station.nil? == true
-      puts 'Train is on the first station.'
-    else
-      puts "Previous station is #{prev_station}"
-    end
+  def move_forward
+    @current_station += 1 if @route.stations[@current_station] != @route.stations.last
+    @route.stations[@current_station]
   end
 
-  def print_current_station
-    puts "Текущая станция #{@current_station.name}"
+  def move_back
+    @current_station -= 1 if @route.stations[@current_station] != @route.stations.first
+    @route.stations[@current_station]
+    # puts @prev_station
   end
 
-  def print_next_station
-    if next_station.nil? == true
-      puts 'Поезд находится на конечной станции'
-    else
-      puts "Следующая станция станция #{next_station}"
-    end
-  end
+  # def print_prev_station
+  #   if prev_station.nil? == true
+  #     puts 'Train is on the first station.'
+  #   else
+  #     puts "Previous station is #{prev_station}"
+  #   end
+  # end
 
-  def go_next_station
-    @current_station = @next_station
-  end
+  # def print_current_station
+  #   puts "Текущая станция #{@current_station.name}"
+  # end
 
-  def go_prev_station
-    @current_station = @prev_station
-  end
+  # def print_next_station
+  #   if next_station.nil? == true
+  #     puts 'Поезд находится на конечной станции'
+  #   else
+  #     puts "Следующая станция станция #{next_station}"
+  #   end
+  # end
+
+  # def go_next_station
+  #   @current_station = @next_station
+  # end
+
+  # def go_prev_station
+  #   @current_station = @prev_station
+  # end
 
   # def moving_from_station(train)
   #   station.depart_train(train)
