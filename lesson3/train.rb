efcc8d9ee8791da05dca38f9dba4d1d61956d31e
Art102 <mckorven@gmail.com>
2018-04-18 07:24:43 +0500

    Add some changes

--- lesson3/train.rb
+++ lesson3/train.rb
@@ -2,7 +2,7 @@
 # require './station.rb'
 
 class Train
-  attr_reader :wagons, :speed, :type
+  attr_reader :wagons, :speed, :type, :current_station
 
   def initialize(number, type, wagons)
     @number = number
@@ -51,10 +51,10 @@ class Train
   end
 
   def accept_route(route)
-    #@route.stations[0].add_train(self)
     @route = route
+    @route.stations[0].add_train(self)
     @current_station = 0
-    puts "Train is on the #{@route.stations[@current_station]} and train is moving on the route #{@route.stations.first.name} - #{@route.stations.last.name}"
+    #puts "Train is on the #{@route.stations[@current_station]} and train is moving on the route #{@route.stations.first.name} - #{@route.stations.last.name}"
   end
 
   # show next station on the route
@@ -79,42 +79,5 @@ class Train
   def move_back
     @current_station -= 1 if @route.stations[@current_station] != @route.stations.first
     @route.stations[@current_station]
-    # puts @prev_station
   end
-
-  # def print_prev_station
-  #   if prev_station.nil? == true
-  #     puts 'Train is on the first station.'
-  #   else
-  #     puts "Previous station is #{prev_station}"
-  #   end
-  # end
-
-  # def print_current_station
-  #   puts "Текущая станция #{@current_station.name}"
-  # end
-
-  # def print_next_station
-  #   if next_station.nil? == true
-  #     puts 'Поезд находится на конечной станции'
-  #   else
-  #     puts "Следующая станция станция #{next_station}"
-  #   end
-  # end
-
-  # def go_next_station
-  #   @current_station = @next_station
-  # end
-
-  # def go_prev_station
-  #   @current_station = @prev_station
-  # end
-
-  # def moving_from_station(train)
-  #   station.depart_train(train)
-  # end
-
-  # def arrival_to_station(train)
-  #   station.add_train(train)
-  # end
 end
