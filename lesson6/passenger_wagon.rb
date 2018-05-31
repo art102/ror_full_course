272bdda7b1f6fa65567e66e54f5ebbbc09c352ad
Art102 <mckorven@gmail.com>
2018-06-01 01:14:08 +0500

    Add some methids to wagon class

--- lesson6/passenger_wagon.rb
+++ lesson6/passenger_wagon.rb
@@ -1,2 +1,13 @@
 class PassengerWagon < Wagon
+  attr_reader :unfree_seats, :all_seats
+
+  def initialize(seats)
+    @all_seats = seats
+    @unfree_seats = 0
+  end
+
+  def reduce_seats
+    @all_seats -= 1
+    @unfree_seats += 1
+  end
 end
