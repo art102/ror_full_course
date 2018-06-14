45a8db93c3ffb0ccabdafdb553cae604094eff9f
ArturDaukaev <daukaevar@bashneft.ru>
2018-06-14 12:05:12 +0500

    Add method to wagon class

--- lesson7/wagon.rb
+++ lesson7/wagon.rb
@@ -2,21 +2,24 @@ require './producer_name.rb'
 
 class Wagon
   include ProducerName
-  attr_reader :capacity, :occupied_capacity, :number, :free_capacity
+  attr_reader :capacity, :occupied_capacity, :number
 
   def initialize(capacity)
     @capacity = capacity
     @occupied_capacity = 0
-    @free_capacity = capacity
     @number = rand(1..1000)
   end
 
   def occupy_capacity(capacity)
-    if capacity <= @free_capacity
+    if capacity <= @capacity && @occupied_capacity != @capacity 
       @occupied_capacity += capacity
-      @free_capacity -= capacity  
+      # @free_capacity -= capacity  
     else
       puts "Capacity has already ended"
     end
+  end
+
+  def free_capacity
+    @capacity - @occupied_capacity
   end
 end
